4bec4a11e69bdd76232962384e9545711120889c
Amirzhan Alimov <>
2021-03-03 14:09:29 +0600

    added new background and configs

--- task2/configs/webpack.common.js
+++ task2/configs/webpack.common.js
@@ -2,6 +2,7 @@ const paths = require('./paths');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const { UnusedFilesWebpackPlugin } = require('unused-files-webpack-plugin');
 
 module.exports = {
     context: paths.src,
@@ -20,8 +21,15 @@ module.exports = {
         }
     },
     plugins: [
+        // plugin does not work with Webpack 5 https://stackoverflow.com/questions/65481943/typeerror-compiler-plugin-is-not-a-function-at-reactloadableplugin-apply
+        // new UnusedFilesWebpackPlugin({
+        //     patterns: [
+        //         'src/components/**/*.jsx',
+        //         'src/pages/**/*.jsx',
+        //         'src/stores/**/*.js'
+        //     ],
+        // }),
         new CleanWebpackPlugin(),
-        // uncomment following, if required to copy some static files
         new CopyWebpackPlugin({
             patterns: [{
                 from: `${paths.public}/assets/favicon.ico`,
@@ -31,51 +39,51 @@ module.exports = {
     ],
     module: {
         rules: [{
-                test: /\.(js|jsx)?$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
+            test: /\.(js|jsx)?$/,
+            exclude: /node_modules/,
+            use: ['babel-loader', 'eslint-loader']
+        },
+        {
+            test: /\.(png|jpg|svg|gif)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: `assets/images/[name].[ext]`
+                }
+            }],
+        },
+        {
+            test: /\.(ttf|woff|woff2|eot)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: `assets/fonts/[name].[ext]`
+                }
+            }],
+        },
+        {
+            test: /\.(s[ac]ss)$/,
+            use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: '',
+                },
             },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: `assets/images/[name].[ext]` 
-                    }
-                }],
+                'css-loader',
+                'sass-loader'
+            ],
+        },
+        {
+            test: /\.(css)$/,
+            use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: '',
+                },
             },
-            {
-                test: /\.(ttf|woff|woff2|eot)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: `assets/fonts/[name].[ext]` 
-                    }
-                }],
-            },
-            {
-                test: /\.(s[ac]ss)$/,
-                use: [{
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '',
-                        },
-                    },
-                    'css-loader',
-                    'sass-loader'
-                ],
-            },
-            {
-                test: /\.(css)$/,
-                use: [{
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '',
-                        },
-                    },
-                    'css-loader'
-                ],
-            }
+                'css-loader'
+            ],
+        }
         ]
     },
 };
